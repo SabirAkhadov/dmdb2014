42a497527e6f7b0edddfa318ee96ccb09ee922ed
koradir <koradir@student.ethz.ch>
2014-03-17 23:11:48 +0100

    Added some comment to test git instructions "commit" and "push" in eclipse.

--- src/ch/ethz/inf/dbproject/HomeServlet.java
+++ src/ch/ethz/inf/dbproject/HomeServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//Changed a file to test git in eclipse.
+
 /**
  * Servlet implementation class HomePage
  */
