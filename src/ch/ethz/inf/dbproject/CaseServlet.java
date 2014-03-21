90a8d13aca2ce12d7f8b7ba65f89ca7860e57a6e
benjamin <benjamin@BenT420>
2014-03-21 08:58:33 +0100

    Test

--- src/ch/ethz/inf/dbproject/CaseServlet.java
+++ src/ch/ethz/inf/dbproject/CaseServlet.java
@@ -17,6 +17,7 @@ import ch.ethz.inf.dbproject.model.Case;
 import ch.ethz.inf.dbproject.util.UserManagement;
 import ch.ethz.inf.dbproject.util.html.BeanTableHelper;
 
+//Comment
 /**
  * Servlet implementation class of Case Details Page
  */
