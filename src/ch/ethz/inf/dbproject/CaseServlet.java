126788e1303fb2487c6cdca0b722bfb86b34e926
richnerb <richnerb@student.ethz.ch>
2014-03-21 09:40:16 +0100

    Clean start

--- src/ch/ethz/inf/dbproject/CaseServlet.java
+++ src/ch/ethz/inf/dbproject/CaseServlet.java
@@ -17,8 +17,6 @@ import ch.ethz.inf.dbproject.model.Case;
 import ch.ethz.inf.dbproject.util.UserManagement;
 import ch.ethz.inf.dbproject.util.html.BeanTableHelper;
 
-//Hoffentlich geht das jetzt ...
-//KONFLIKT MIT ROGER
 /**
  * Servlet implementation class of Case Details Page
  */
